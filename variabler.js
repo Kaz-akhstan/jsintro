514379999aeace7363bf6ac2372b1f5744c71399
= <=>
2023-09-05 14:26:07 +0200

    Commit

---
+++ variabler.js
@@ -0,0 +1,17 @@
+let name = "Rasmus"
+console.log(typeof(name))
+const greeting = `Hej ${name}`
+console.log(greeting)
+let age = 18
+console.log(typeof(age))
+console.log(Infinity*2)
+let x = Number.MAX_VALUE
+console.log(x)
+console.log(name + x * 2)
+let y = x * 2 + 1
+console.log(typeof(y))
+console.log(isNaN(y))
+
+function CL(val) {
+    console.log(val)
+}
